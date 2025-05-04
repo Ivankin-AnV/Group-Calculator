ed74ac632cc80040dafc73c36817326c35080cb4
toxai <toni.ivankin.04@mail.ru>
2025-05-04 15:03:49 +0500

    small fix

--- src/models/calculatorModel.js
+++ src/models/calculatorModel.js
@@ -1,12 +1,12 @@
 export function fractionToDecimal(fraction) {
-    const [numerator, denominator] = fraction.split('/').map(Number);
-    if (denominator === 0) throw new Error("Деление на ноль!");
-    return numerator / denominator;
-  }
+  const [numerator, denominator] = fraction.split('/').map(n => parseFloat(n.replace(",", ".")));
+  if (denominator === 0) throw new Error("Деление на ноль!");
+  return numerator / denominator;
+}
   
   export function calculateExpression(input) {
-    const processed = input.replace(",", ".");
-    const regex = /(\d+\/\d+)/g;
+    const processed = input.replace(/,/g, ".");
+    const regex = /\d+\/\d+(?:\.\d+)?/g;
     const evaluated = processed.replace(regex, match =>
       fractionToDecimal(match).toString()
     );
