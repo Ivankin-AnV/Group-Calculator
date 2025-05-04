52ee4f9b5f778c6b3459d433a2de53368a87ba5f
toxai <toni.ivankin.04@mail.ru>
2025-05-04 15:09:19 +0500

    small fix

--- src/models/calculatorModel.js
+++ src/models/calculatorModel.js
@@ -1,9 +1,9 @@
 export function fractionToDecimal(fraction) {
   const [numerator, denominator] = fraction.split('/').map(n => parseFloat(n.replace(",", ".")));
-  if (denominator === 0) throw new Error("Деление на ноль!");
+  if (denominator === 0) throw new Error("Деление на ноль");
   return numerator / denominator;
 }
-  
+
   export function calculateExpression(input) {
     const processed = input.replace(/,/g, ".");
     const regex = /\d+\/\d+(?:\.\d+)?/g;
