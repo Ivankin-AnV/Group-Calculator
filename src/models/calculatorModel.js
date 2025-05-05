90c9e4cfe895c851b704e9e77ae0ac066256cada
toxai <toni.ivankin.04@mail.ru>
2025-05-05 15:41:58 +0500

    исправление ошибки

--- src/models/calculatorModel.js
+++ src/models/calculatorModel.js
@@ -1,15 +1,16 @@
 export function fractionToDecimal(fraction) {
-  const [numerator, denominator] = fraction.split('/').map(n => parseFloat(n.replace(",", ".")));
+  const [numerator, denominator] = fraction
+    .split('/')
+    .map(n => parseFloat(n.replace(",", ".")));
   if (denominator === 0) throw new Error("Деление на ноль");
   return numerator / denominator;
 }
 
-  export function calculateExpression(input) {
-    const processed = input.replace(/,/g, ".");
-    const regex = /\d+\/\d+(?:\.\d+)?/g;
-    const evaluated = processed.replace(regex, match =>
-      fractionToDecimal(match).toString()
-    );
-    return eval(evaluated);
-  }
-  
+export function calculateExpression(input) {
+  const processed = input.replace(/,/g, ".");
+  const regex = /[0-9.]+\/[0-9.]+/g;
+  const evaluated = processed.replace(regex, match =>
+    fractionToDecimal(match).toString()
+  );
+  return eval(evaluated);
+}
